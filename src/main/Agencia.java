96cf3a913e057519dbe1c39d6dcc04a8c85f5318
Gilson Trombetta Magro <gilsontm@users.noreply.github.com>
2019-03-19 11:15:57 -0300

    Classes 'Agencia' e 'Cliente' adicionadas.

---
+++ src/main/Agencia.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Agencia {
+	
+	private Integer numero;
+	private String nome;
+	private String endereco;
+	private ArrayList<Cliente> clientes = new ArrayList<Cliente>();
+	private ArrayList<Conta> contas = new ArrayList<Conta>();
+	
+	public Agencia() {
+		
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public ArrayList<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(ArrayList<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public ArrayList<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(ArrayList<Conta> contas) {
+		this.contas = contas;
+	}
+}
---
+++ src/main/Cliente.java
@@ -0,0 +1,56 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Cliente {
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
+	private ArrayList<Conta> contas = new ArrayList<Conta>();
+	
+	public Cliente() {
+		
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public ArrayList<Conta> getContas() {
+		return contas;
+	}
+
+	public void setContas(ArrayList<Conta> contas) {
+		this.contas = contas;
+	}
+}
